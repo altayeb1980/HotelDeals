b4c96669ccf65b5d9122b22b5fee58a5ed07e842
Altayeb Saadeh <loay.saada@gmail.com>
2018-09-08 01:02:54 +0200

    apply sunbmit action to the main page, adding the json model objects, add the endpoint url to the properties file

---
+++ HotelDeals/src/main/java/com/epam/service/DefaultHotelService.java
@@ -0,0 +1,42 @@
+package com.epam.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.epam.model.HotelDeal;
+import com.epam.model.SearchCriteria;
+
+@Service
+public class DefaultHotelService implements HotelService{
+
+	@Value("${hotel.deals.url}")
+	private String hotelDealsUrl;
+	
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	@Override
+	public HotelDeal findAll() {
+		return restTemplate.getForObject(
+				hotelDealsUrl, HotelDeal.class);
+	}
+
+
+	@Override
+	public HotelDeal findByCriteria(SearchCriteria searchCriteria) {
+		UriComponentsBuilder builder = UriComponentsBuilder
+			    .fromUriString(hotelDealsUrl)
+			    // Add query parameter
+			    .queryParam("destinationName", searchCriteria.getDestinationName());
+		
+		
+		HotelDeal hotelDeal = restTemplate.getForObject(builder.toUriString(), HotelDeal.class);
+		return hotelDeal;
+	}
+
+}
--- HotelDeals/src/main/java/com/epam/service/HotelService.java
+++ HotelDeals/src/main/java/com/epam/service/HotelService.java
@@ -1,9 +1,13 @@
 package com.epam.service;
 
 import com.epam.model.HotelDeal;
+import com.epam.model.SearchCriteria;
 
 public interface HotelService {
 
 	public HotelDeal findAll();
+	public HotelDeal findByCriteria(SearchCriteria searchCriteria);
+	
+	
 	
 }
