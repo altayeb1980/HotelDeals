a290849daeb72ec84708b5010a69cc3c3bc6a1e7
Altayeb Saadeh <loay.saada@gmail.com>
2018-09-07 18:55:48 +0200

    add HotelController to handle the default page, and add message.properties for resource bundle messages with adding the template and default page

---
+++ HotelDeals/src/main/java/com/epam/controller/HotelController.java
@@ -0,0 +1,23 @@
+package com.epam.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.epam.service.HotelService;
+
+
+@Controller
+public class HotelController {
+	@Autowired
+	private HotelService hotelService;
+	
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("index");
+		return modelAndView;
+	}
+	
+	
+}
