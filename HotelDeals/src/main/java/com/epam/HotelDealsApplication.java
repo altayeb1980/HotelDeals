81fb1a92a732e79d70aa36dc627714e08e0f174a
Altayeb Saadeh <loay.saada@gmail.com>
2018-09-07 18:17:57 +0200

    Initial checkin of HotelDeals project

---
+++ HotelDeals/src/main/java/com/epam/HotelDealsApplication.java
@@ -0,0 +1,12 @@
+package com.epam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelDealsApplication.class, args);
+	}
+}
