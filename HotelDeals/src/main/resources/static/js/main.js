dce2321024c9e5296c0c45aeb7274b58713e14a0
Altayeb Saadeh <loay.saada@gmail.com>
2018-09-08 12:49:30 +0200

    add validation to the forum fields

---
+++ HotelDeals/src/main/resources/static/js/main.js
@@ -0,0 +1,44 @@
+$("#mainForm").submit(function() {
+	var minTotalRate = $("#minTotalRate").val();
+	var maxTotalRate = $("#maxTotalRate").val();
+
+	var minGuestRating = $("#minGuestRating").val();
+	var maxGuestRating = $("#maxGuestRating").val();
+
+	var minStarRating = $("#minStarRating").val();
+	var maxStarRating = $("#maxStarRating").val();
+
+	if (minStarRating != null && maxStarRating != null) {
+		if (minStarRating > maxStarRating) {
+			alert("maxStarRating should be greater than minStarRating");
+			return false;
+		}
+	}
+
+	if (minTotalRate != null && maxTotalRate != null) {
+		if (minTotalRate > maxTotalRate) {
+			alert("maxTotalRate should be greater than minTotalRate");
+			return false;
+		}
+	}
+
+	if (minGuestRating != null && maxGuestRating != null) {
+		if (minGuestRating > maxGuestRating) {
+			alert("maxGuestRating should be greater than minGuestRating");
+			return false;
+		}
+	}
+	return true;
+});
+
+$('#destinationName').on(
+		'keypress',
+		function(event) {
+			var regex = new RegExp("^[0-9a-zA-Z_ \b]+$");
+			var key = String.fromCharCode(!event.charCode ? event.which
+					: event.charCode);
+			if (!regex.test(key)) {
+				event.preventDefault();
+				return false;
+			}
+		});
